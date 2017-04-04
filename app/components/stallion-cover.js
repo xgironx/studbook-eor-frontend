04466cc6a474d835fe4b81c3a224c44316820951
xgironx <xgironx@gmail.com>
2017-04-04 13:27:12 -0400

    added Gemfile

---
+++ app/components/stallion-cover.js
@@ -0,0 +1,7 @@
+// {{!-- ################################
+// 201704041333L   EL MARTES   JAY @ GA --}}
+
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+});
--- app/routes/stallions.js
+++ app/routes/stallions.js
@@ -2,3 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 });
+
+
+
+                                    // #####################################
+                                    // 201704041222L   EL MARTES   JAY @ GA
+                                    // https://emberigniter.com/building-user-interface-around-ember-data-app/
+                                    // THIS LEADS TO IN CHROME
+                                    // GET http://localhost:4200/favicon.ico 500 (Internal Server Error)
+                                    // TypeError: studbook-eor-frontend/routes/stallions.js: Line 5: Duplicate declaration "Ember"
+                                    // export default Ember.Route.extend({
+                                    // });
+                                    // import Ember from 'ember';
+                                    // export default Ember.Route.extend({
+                                    //   model() {
+                                    //     at File.errorWithNode (/User
+                                    // OR
+                                    // GET http://localhost:4200/stallions 406 (Not Acceptable)
+// import Ember from 'ember';
+// export default Ember.Route.extend({
+//   model() {
+//     return this.store.findAll('stallion');
+//   }
+// });
